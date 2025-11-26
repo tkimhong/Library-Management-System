8d6899aacadbe8524e69dd0c7bb0cbf848d49681
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-11-26 23:54:49 +0700

    Add BorrowRecord model boilerplate

--- src/models/BorrowRecord.java
+++ src/models/BorrowRecord.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.time.LocalDate;
+
 public class BorrowRecord {
+    // TODO (Kimhor): Add these fields
+    // - User user
+    // - Book book
+    // - LocalDate borrowDate
+    // - LocalDate dueDate
+    // - LocalDate returnDate (can be null if not returned)
+    // - boolean isOverdue
+
+    // TODO: Add constructor
+
+    // TODO: Add getters and setters
+
+    // TODO: Add method to check if overdue
 }

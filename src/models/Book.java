8acaf8dd97afdcf8398b9cb7515f6d393c325b3a
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-11-26 23:54:24 +0700

    Add Book model boilerplate

--- src/models/Book.java
+++ src/models/Book.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Book {
+    // TODO (Morngdyza): Add these fields
+    // - String title
+    // - String author
+    // - String genre
+    // - String ISBN
+    // - int copiesAvailable
+    // - int totalCopies
+
+    // TODO: Add constructor
+
+    // TODO: Add getters and setters
+
+    // TODO: Add toString() method for displaying book info
 }

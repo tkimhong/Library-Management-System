11400053cdfc16f314b7e7a39a5704dfb54e640f
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-11-26 23:53:52 +0700

    Add User model boilerplate

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Library Book Borrowing System");
-        System.out.println("============================");
+        System.out.println("=============================");
 
         // TODO: Init MainMenu and start app
         // MainMenu menu = new MainMenu();
--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,15 @@
 package models;
 
 public class User {
+    // TODO (Mengkheang): Add these fields
+    // - String name
+    // - String studentID
+    // - String email
+    // - boolean isActive
+
+    // TODO: Add constructor
+
+    // TODO: Add getters and setters
+
+    // TODO: Add toString() method for displaying user info
 }

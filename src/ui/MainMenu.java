91fbd18eb3f1387808b70f2baf6cae7a3f991edf
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-12-01 21:49:04 +0700

    Add main menu loop to MainMenu.start()
    Implement main nav loop with 5 options.
    Switch statement route to submenu methods and have input validation for
    invalid choices.
    Exit condition to terminate program.

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -28,5 +28,42 @@ public class MainMenu {
         // 3. Borrowing System
         // 4. View Overdue Books
         // 5. Exit
+
+        boolean running = true;
+
+        while (running){
+            System.out.println("\n=== LIBRARY MANAGEMENT SYSTEM ===");
+            System.out.println("1. User Management");
+            System.out.println("2. Book Management");
+            System.out.println("3. Borrowing System");
+            System.out.println("4. View Overdue Books");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // to have an empty space
+
+            switch (choice){
+                case 1:
+                    userManagementMenu();
+                    break;
+                case 2:
+                    bookManagementMenu();
+                    break;
+                case 3:
+                    borrowingMenu();
+                    break;
+                case 4:
+                    viewOverdueBooks();
+                    break;
+                case 5:
+                    running = false;
+                    System.out.println("Bye bye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice, try again!");
+            }
+        }
+
     }
 }

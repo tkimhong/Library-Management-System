f8c53ad6cb48c50ea21458259ec82b64c8bc3281
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-12-01 23:28:13 +0700

    Init MainMenu and start app

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import ui.MainMenu;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Library Book Borrowing System");
         System.out.println("=============================");
 
-        // TODO: Init MainMenu and start app
-        // MainMenu menu = new MainMenu();
-        // menu.start();
+        MainMenu menu = new MainMenu();
+        menu.start();
     }
 }

702866d671f5d0720123101c10333a389c2034cd
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-12-19 01:08:16 +0700

    Delete TODO comments from User.java

--- src/models/User.java
+++ src/models/User.java
@@ -2,25 +2,18 @@ package models;
 
 
 public class User {
-    // TODO (Mengkheang): Add these fields
-    // - String name
-    // - String studentID
-    // - String email
-    // - boolean isActive
     private String name;
     private String studentID;
     private String email;
     private boolean isActive;
 
 
-    // TODO: Add constructor
     public User (String name, String studentID, String email){
         this.name = name;
         this.studentID = studentID;
         this.email = email;
         this.isActive = true;
     }
-    // TODO: Add getters and setters
     public String getName() {
         return name;
     }
@@ -53,7 +46,6 @@ public class User {
     public void setActive(boolean active) {
         this.isActive = active;
     }
-    // TODO: Add toString() method for displaying user info
     public String toString() {
         return "Name: " + name +
                 "\nStudent ID: " + studentID +
